9347e1522f46ee3a423b21ff61a7f4ba0f2306a3
corech <co.rech@hotmail.com>
2026-06-10 15:08:51 -0300

    Atualizacao_2026-06-10

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
   apps: [
-    {
+    /*{
       name: 'webhook-whatsapp-meta',
       script: './processwhatsapp.js',
       cwd: path.join(__dirname, 'backend'),
@@ -27,7 +27,7 @@ module.exports = {
         NODE_ENV: 'development',
         PORT: 3000,
       },
-    },
+    },*/
     {
       name: 'app-server',
       script: './app.js',
