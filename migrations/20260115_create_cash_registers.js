1d2a237025d31012867bfa99f2f865398a8e603e
corech <co.rech@hotmail.com>
2026-06-22 16:22:38 -0300

    Atualizacao_2026-06-22

---
+++ migrations/20260115_create_cash_registers.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Compat migration stub restored to keep knex migration history consistent.
+ * Original migration file was missing from the repository.
+ */
+exports.up = async function up() {
+  // no-op
+};
+
+exports.down = async function down() {
+  // no-op
+};
